714f79e132ceaf3f0c55606f72150d0803e77e8b
Klazen <cmurphy1337@live.com>
2019-12-04 00:05:29 -0600

    Day 4:1 complete

--- day4.go
+++ day4.go
@@ -1,5 +1,41 @@
 package main
 
+import (
+	"strconv"
+	"strings"
+)
+
 func Challenge4_1(rangeStr string) string {
-	return ""
+	valStrs := strings.Split(rangeStr, "-")
+	min, _ := strconv.Atoi(valStrs[0])
+	max, _ := strconv.Atoi(valStrs[1])
+
+	count := 0
+	for i := min; i <= max; i++ {
+		curStr := strconv.Itoa(i)
+		if MeetsRules(curStr) {
+			count++
+		}
+	}
+	return strconv.Itoa(count)
+}
+
+func MeetsRules(val string) bool {
+	hasAdjacent := false
+	monoIncrease := true
+	curMin := 0
+	last := -1
+	for _, char := range val {
+		cVal := int(char - '0')
+		if cVal < curMin {
+			monoIncrease = false
+			break
+		}
+		if cVal == last {
+			hasAdjacent = true
+		}
+		last = cVal
+		curMin = max(curMin, cVal)
+	}
+	return hasAdjacent && monoIncrease
 }
--- day4_test.go
+++ day4_test.go
@@ -6,7 +6,25 @@ import (
 
 func TestDay4_1(t *testing.T) {
 	result := Challenge4_1("193651-649729")
-	if result != "" {
-		t.Errorf("Result was incorrect, got: %s, want: %s.", result, "")
+	if result != "1605" {
+		t.Errorf("Result was incorrect, got: %s, want: %s.", result, "1605")
+	}
+}
+
+func TestExample4_1_1(t *testing.T) {
+	if !MeetsRules("111111") {
+		t.Errorf("Expected valid")
+	}
+}
+
+func TestExample4_1_2(t *testing.T) {
+	if MeetsRules("223450") {
+		t.Errorf("Expected invalid")
+	}
+}
+
+func TestExample4_1_3(t *testing.T) {
+	if MeetsRules("123789") {
+		t.Errorf("Expected invalid")
 	}
 }
