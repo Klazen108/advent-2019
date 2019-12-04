df769e22fc017772575051a9c2beca030a1c96c7
Klazen <cmurphy1337@live.com>
2019-12-03 21:09:29 -0600

    Day 2:1 complete

--- day2.go
+++ day2.go
@@ -1,5 +1,64 @@
 package main
 
-func Challenge2_1(program string) []int {
-	return make([]int, 0)
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func Challenge2_1(program string) string {
+	memory := CsvToIntSlice(program)
+	//fix program
+	memory[1] = 12
+	memory[2] = 2
+	RunProgram(&memory)
+	fmt.Fprintf(os.Stderr, "Value at position 0: %d\n", memory[0])
+	return strconv.FormatInt(memory[0], 10)
+}
+
+func RunProgram(opcodes *[]int64) {
+	pc := 0
+	for {
+		opcode := (*opcodes)[pc]
+		switch opcode {
+		case 1:
+			operand1Addr := (*opcodes)[pc+1]
+			operand2Addr := (*opcodes)[pc+2]
+			targetAddr := (*opcodes)[pc+3]
+
+			operand1 := (*opcodes)[operand1Addr]
+			operand2 := (*opcodes)[operand2Addr]
+
+			result := operand1 + operand2
+			(*opcodes)[targetAddr] = result
+		case 2:
+			operand1Addr := (*opcodes)[pc+1]
+			operand2Addr := (*opcodes)[pc+2]
+			targetAddr := (*opcodes)[pc+3]
+
+			operand1 := (*opcodes)[operand1Addr]
+			operand2 := (*opcodes)[operand2Addr]
+
+			result := operand1 * operand2
+			(*opcodes)[targetAddr] = result
+		case 99:
+			return
+		}
+		pc += 4
+	}
+}
+
+func CsvToIntSlice(csv string) []int64 {
+	csvSlice := strings.Split(csv, ",")
+	intSlice := make([]int64, len(csvSlice))
+	for i, elem := range csvSlice {
+		intval, err := strconv.ParseInt(elem, 10, 64)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Invalid value in input: '%s'\n", elem)
+			os.Exit(103)
+		}
+		intSlice[i] = intval
+	}
+	return intSlice
 }
--- day2_test.go
+++ day2_test.go
@@ -6,24 +6,54 @@ import (
 	"testing"
 )
 
-func TestChallenge2_1Example(t *testing.T) {
-	memory := Challenge2_1("1,9,10,3,2,3,11,0,99,30,40,50")
+func TestDay2_1(t *testing.T) {
+	result := Challenge2_1("1,0,0,3,1,1,2,3,1,3,4,3,1,5,0,3,2,6,1,19,1,19,9,23,1,23,9,27,1,10,27,31,1,13,31,35,1,35,10,39,2,39,9,43,1,43,13,47,1,5,47,51,1,6,51,55,1,13,55,59,1,59,6,63,1,63,10,67,2,67,6,71,1,71,5,75,2,75,10,79,1,79,6,83,1,83,5,87,1,87,6,91,1,91,13,95,1,95,6,99,2,99,10,103,1,103,6,107,2,6,107,111,1,13,111,115,2,115,10,119,1,119,5,123,2,10,123,127,2,127,9,131,1,5,131,135,2,10,135,139,2,139,9,143,1,143,2,147,1,5,147,0,99,2,0,14,0")
+	if result != "5534943" {
+		t.Errorf("Result was incorrect, got: %s, want: %s.", result, "5534943")
+	}
+}
 
+func TestRunProgram_Example1(t *testing.T) {
+	memory := CsvToIntSlice("1,0,0,0,99")
+	RunProgram(&memory)
+	msmCsv := getMemoryAsCsv(memory)
+	if msmCsv != "2,0,0,0,99" {
+		t.Errorf("Memory was incorrect, got: %s, want: %s.", msmCsv, "2,0,0,0,99")
+	}
+}
+func TestRunProgram_Example2(t *testing.T) {
+	memory := CsvToIntSlice("2,3,0,3,99")
+	RunProgram(&memory)
+	msmCsv := getMemoryAsCsv(memory)
+	if msmCsv != "2,3,0,6,99" {
+		t.Errorf("Memory was incorrect, got: %s, want: %s.", msmCsv, "2,3,0,6,99")
+	}
+}
+func TestRunProgram_Example3(t *testing.T) {
+	memory := CsvToIntSlice("2,4,4,5,99,0")
+	RunProgram(&memory)
+	msmCsv := getMemoryAsCsv(memory)
+	if msmCsv != "2,4,4,5,99,9801" {
+		t.Errorf("Memory was incorrect, got: %s, want: %s.", msmCsv, "2,4,4,5,99,9801")
+	}
+}
+func TestRunProgram_Example4(t *testing.T) {
+	memory := CsvToIntSlice("1,1,1,4,99,5,6,0,99")
+	RunProgram(&memory)
+	msmCsv := getMemoryAsCsv(memory)
+	if msmCsv != "30,1,1,4,2,5,6,0,99" {
+		t.Errorf("Memory was incorrect, got: %s, want: %s.", msmCsv, "30,1,1,4,2,5,6,0,99")
+	}
+}
+
+func getMemoryAsCsv(memory []int64) string {
 	valuesText := []string{}
 
-	// Create a string slice using strconv.Itoa.
-	// ... Append strings to it.
 	for i := range memory {
 		number := memory[i]
-		text := strconv.Itoa(number)
+		text := strconv.FormatInt(number, 10)
 		valuesText = append(valuesText, text)
 	}
 
-	// Join our string slice.
-	result := strings.Join(valuesText, ",")
-
-	if result != "30,1,1,4,2,5,6,0,99" {
-		result := strings.Join(valuesText, ",")
-		t.Errorf("Memory was incorrect, got: %s, want: %s.", result, "30,1,1,4,2,5,6,0,99")
-	}
+	return strings.Join(valuesText, ",")
 }
