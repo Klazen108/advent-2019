b08af7ed9207ad35131f8b6e9c5967c249fc074b
Klazen <cmurphy1337@live.com>
2019-12-03 19:33:47 -0600

    Challenge 1 complete

--- main.go
+++ main.go
@@ -1,7 +1,50 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"strconv"
+	"strings"
+)
 
 func main() {
-	fmt.Printf("hello, world\n")
+	challenge1()
+}
+
+func challenge1() {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Enter your list of module masses, in csv format:\n>")
+	text, err := reader.ReadString('\n')
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error reading module masses: %v\n", err)
+		os.Exit(101)
+	}
+	moduleMassesStr := strings.Split(strings.Trim(text, "\r\n"), ",")
+	moduleMasses := make([]float64, len(moduleMassesStr))
+	for i, elem := range moduleMassesStr {
+		moduleMasses[i], err = strconv.ParseFloat(elem, 64)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Invalid module mass: '%s'\n", elem)
+			os.Exit(102)
+		}
+	}
+
+	spacecraftFuel := getSpacecraftFuel(moduleMasses)
+	fmt.Fprintf(os.Stderr, "Fuel required: %f\n", spacecraftFuel)
+}
+
+func getModuleFuel(moduleMass float64) float64 {
+	return math.Floor(moduleMass/3) - 2
+}
+
+func getSpacecraftFuel(moduleMasses []float64) float64 {
+	moduleFuels := make([]float64, len(moduleMasses))
+	total := 0.0
+	for i, elem := range moduleMasses {
+		moduleFuels[i] = getModuleFuel(elem)
+		total += moduleFuels[i]
+	}
+	return total
 }
