1bb8c9a36417c17ebf822108829ab715ad678b44
Klazen <cmurphy1337@live.com>
2019-12-04 00:46:37 -0600

    Day 4:2 complete

--- day4.go
+++ day4.go
@@ -20,6 +20,21 @@ func Challenge4_1(rangeStr string) string {
 	return strconv.Itoa(count)
 }
 
+func Challenge4_2(rangeStr string) string {
+	valStrs := strings.Split(rangeStr, "-")
+	min, _ := strconv.Atoi(valStrs[0])
+	max, _ := strconv.Atoi(valStrs[1])
+
+	count := 0
+	for i := min; i <= max; i++ {
+		curStr := strconv.Itoa(i)
+		if MeetsRules2(curStr) {
+			count++
+		}
+	}
+	return strconv.Itoa(count)
+}
+
 func MeetsRules(val string) bool {
 	hasAdjacent := false
 	monoIncrease := true
@@ -38,4 +53,31 @@ func MeetsRules(val string) bool {
 		curMin = max(curMin, cVal)
 	}
 	return hasAdjacent && monoIncrease
+}
+
+func MeetsRules2(val string) bool {
+	if !MeetsRules(val) {
+		return false
+	}
+
+	runs := make(map[int]int)
+	for _, char := range val {
+		cVal := int(char - '0')
+
+		run := 0
+		mapRun, ok := runs[cVal]
+		if ok {
+			run = mapRun
+		}
+		run++
+		runs[cVal] = run
+	}
+
+	for _, count := range runs {
+		if count == 2 {
+			return true
+		}
+	}
+
+	return false
 }
--- day4_test.go
+++ day4_test.go
@@ -11,6 +11,13 @@ func TestDay4_1(t *testing.T) {
 	}
 }
 
+func TestDay4_2(t *testing.T) {
+	result := Challenge4_2("193651-649729")
+	if result != "1605" {
+		t.Errorf("Result was incorrect, got: %s, want: %s.", result, "1605")
+	}
+}
+
 func TestExample4_1_1(t *testing.T) {
 	if !MeetsRules("111111") {
 		t.Errorf("Expected valid")
@@ -26,5 +33,47 @@ func TestExample4_1_2(t *testing.T) {
 func TestExample4_1_3(t *testing.T) {
 	if MeetsRules("123789") {
 		t.Errorf("Expected invalid")
+	}
+}
+
+func TestExample4_2_1(t *testing.T) {
+	if !MeetsRules2("112233") {
+		t.Errorf("Expected valid")
+	}
+}
+
+func TestExample4_2_2(t *testing.T) {
+	if MeetsRules2("123444") {
+		t.Errorf("Expected invalid")
+	}
+}
+
+func TestExample4_2_3(t *testing.T) {
+	if !MeetsRules2("111122") {
+		t.Errorf("Expected valid")
+	}
+}
+
+func TestExample4_2_x1(t *testing.T) {
+	if !MeetsRules2("223333") {
+		t.Errorf("Expected valid")
+	}
+}
+
+func TestExample4_2_x2(t *testing.T) {
+	if !MeetsRules2("344555") {
+		t.Errorf("Expected valid")
+	}
+}
+
+func TestExample4_2_x3(t *testing.T) {
+	if MeetsRules2("123456") {
+		t.Errorf("Expected invalid")
+	}
+}
+
+func TestExample4_2_x4(t *testing.T) {
+	if !MeetsRules2("113456") {
+		t.Errorf("Expected valid")
 	}
 }
