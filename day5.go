7f32ede3c9837e46c78390f2cc6d5d32c9bce1c0
Klazen <cmurphy1337@live.com>
2019-12-04 23:43:59 -0600

    Day 5:2 Complete

--- day5.go
+++ day5.go
@@ -4,75 +4,82 @@ import (
 	"strconv"
 )
 
-func Challenge5_1(program string) string {
+func Challenge5_1(program string, input int64) string {
 	memory := CsvToIntSlice(program)
-	output := RunProgram5(&memory)
+	output := RunProgram5(&memory, input)
 	return strconv.Itoa(int(output))
 }
 
-func RunProgram5(opcodes *[]int64) int64 {
+func RunProgram5(opcodes *[]int64, input int64) int64 {
 	result := int64(0)
 	pc := 0
 	for {
 		opcode := Opcode((*opcodes)[pc])
 		switch opcode.Code() {
 		case 1: //add
-			var operand1 int64
-			if opcode.Mode(0) == Immediate {
-				operand1 = (*opcodes)[pc+1]
-			} else {
-				operand1Addr := (*opcodes)[pc+1]
-				operand1 = (*opcodes)[operand1Addr]
-			}
-			var operand2 int64
-			if opcode.Mode(1) == Immediate {
-				operand2 = (*opcodes)[pc+2]
-			} else {
-				operand2Addr := (*opcodes)[pc+2]
-				operand2 = (*opcodes)[operand2Addr]
-			}
-
+			operand1 := GetOperandVal(opcode, opcodes, pc, 0)
+			operand2 := GetOperandVal(opcode, opcodes, pc, 1)
 			targetAddr := (*opcodes)[pc+3]
 
 			res := operand1 + operand2
 			(*opcodes)[targetAddr] = res
 			pc += opcode.ParmCount()
 		case 2: //multiply
-			var operand1 int64
-			if opcode.Mode(0) == Immediate {
-				operand1 = (*opcodes)[pc+1]
-			} else {
-				operand1Addr := (*opcodes)[pc+1]
-				operand1 = (*opcodes)[operand1Addr]
-			}
-			var operand2 int64
-			if opcode.Mode(1) == Immediate {
-				operand2 = (*opcodes)[pc+2]
-			} else {
-				operand2Addr := (*opcodes)[pc+2]
-				operand2 = (*opcodes)[operand2Addr]
-			}
-
+			operand1 := GetOperandVal(opcode, opcodes, pc, 0)
+			operand2 := GetOperandVal(opcode, opcodes, pc, 1)
 			targetAddr := (*opcodes)[pc+3]
 
 			res := operand1 * operand2
 			(*opcodes)[targetAddr] = res
 			pc += opcode.ParmCount()
 		case 3: //input
-			input := int64(1)
 			targetAddr := (*opcodes)[pc+1]
 			(*opcodes)[targetAddr] = input
 			pc += opcode.ParmCount()
 		case 4: //output
-			var operand1 int64
-			if opcode.Mode(0) == Immediate {
-				operand1 = (*opcodes)[pc+1]
-			} else {
-				operand1Addr := (*opcodes)[pc+1]
-				operand1 = (*opcodes)[operand1Addr]
-			}
+			operand1 := GetOperandVal(opcode, opcodes, pc, 0)
 
 			result = operand1
+			pc += opcode.ParmCount()
+		case 5:
+			operand1 := GetOperandVal(opcode, opcodes, pc, 0)
+			operand2 := GetOperandVal(opcode, opcodes, pc, 1)
+			if operand1 != 0 {
+				pc = int(operand2)
+			} else {
+				pc += opcode.ParmCount()
+			}
+		case 6:
+			operand1 := GetOperandVal(opcode, opcodes, pc, 0)
+			operand2 := GetOperandVal(opcode, opcodes, pc, 1)
+			if operand1 == 0 {
+				pc = int(operand2)
+			} else {
+				pc += opcode.ParmCount()
+			}
+		case 7:
+			operand1 := GetOperandVal(opcode, opcodes, pc, 0)
+			operand2 := GetOperandVal(opcode, opcodes, pc, 1)
+			targetAddr := (*opcodes)[pc+3]
+
+			if operand1 < operand2 {
+				(*opcodes)[targetAddr] = 1
+			} else {
+				(*opcodes)[targetAddr] = 0
+			}
+
+			pc += opcode.ParmCount()
+		case 8:
+			operand1 := GetOperandVal(opcode, opcodes, pc, 0)
+			operand2 := GetOperandVal(opcode, opcodes, pc, 1)
+			targetAddr := (*opcodes)[pc+3]
+
+			if operand1 == operand2 {
+				(*opcodes)[targetAddr] = 1
+			} else {
+				(*opcodes)[targetAddr] = 0
+			}
+
 			pc += opcode.ParmCount()
 		case 99:
 			return result
@@ -82,6 +89,17 @@ func RunProgram5(opcodes *[]int64) int64 {
 		}
 	}
 	return result
+}
+
+func GetOperandVal(op Opcode, memory *[]int64, pc int, parmIndex int) int64 {
+	var operand1 int64
+	if op.Mode(parmIndex) == Immediate {
+		operand1 = (*memory)[pc+1+parmIndex]
+	} else {
+		operand1Addr := (*memory)[pc+1+parmIndex]
+		operand1 = (*memory)[operand1Addr]
+	}
+	return operand1
 }
 
 type Opcode int64
@@ -124,6 +142,14 @@ func (o Opcode) ParmCount() int {
 		return 2
 	case 4:
 		return 2
+	case 5:
+		return 3
+	case 6:
+		return 3
+	case 7:
+		return 4
+	case 8:
+		return 4
 	case 99:
 		return 1
 	}
