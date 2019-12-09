a5b8b6fc509a2eb8b345d912d83e7d72346b5a1e
Klazen <cmurphy1337@live.com>
2019-12-08 22:44:25 -0600

    Day 8:2 Complete

--- day8.go
+++ day8.go
@@ -5,12 +5,12 @@ import (
 	"strconv"
 )
 
-func Challenge8_1(imageData []byte) string {
-	image := DecodeSIF(imageData, 25, 6)
-	minLayer := image[0]
+func Challenge8_1(imageData []byte, width int, height int) string {
+	image := DecodeSIF(imageData, width, height)
+	minLayer := image.layers[0]
 	//minLayerI := 0
 	minCount := minLayer.CountDigits(0)
-	for _, layer := range image {
+	for _, layer := range image.layers {
 		curCount := layer.CountDigits(0)
 		//fmt.Printf("Cur layer: %d zeroes: %d\n", i, curCount)
 		if curCount < minCount {
@@ -23,14 +23,24 @@ func Challenge8_1(imageData []byte) string {
 	return strconv.Itoa(minLayer.CountDigits(1) * minLayer.CountDigits(2))
 }
 
-type SpaceImage []SIFLayer
+func Challenge8_2(imageData []byte, width int, height int) {
+	image := DecodeSIF(imageData, width, height)
+	image.PrintImage()
+	//image.DumpImage()
+}
+
+type SpaceImage struct {
+	layers []SIFLayer
+	width  int
+	height int
+}
 
 type SIFLayer []byte
 
 func DecodeSIF(bytes []byte, width int, height int) SpaceImage {
 	layers := make([]SIFLayer, 0)
 	layerSize := width * height
-	for offset := 0; offset < len(bytes)-layerSize; offset += layerSize {
+	for offset := 0; offset <= len(bytes)-layerSize; offset += layerSize {
 		layers = append(layers, bytes[offset:offset+layerSize])
 	}
 	for layerI, layer := range layers {
@@ -42,7 +52,13 @@ func DecodeSIF(bytes []byte, width int, height int) SpaceImage {
 			panic(fmt.Sprintf("Bad layer length in layer %d", layerI))
 		}
 	}
-	return layers
+	return SpaceImage{layers, width, height}
+}
+
+func (image SpaceImage) DumpImage() {
+	for _, layer := range image.layers {
+		fmt.Printf("%+v\n", layer)
+	}
 }
 
 func (layer SIFLayer) CountDigits(digit int) int {
@@ -53,4 +69,63 @@ func (layer SIFLayer) CountDigits(digit int) int {
 		}
 	}
 	return count
+}
+
+func (image SpaceImage) PrintImage() {
+	for y := 0; y < image.height; y++ {
+		for x := 0; x < image.width; x++ {
+			i := y*image.width + x
+
+			abort := false
+			for _, layer := range image.layers {
+				switch layer[i] {
+				case 1:
+					fmt.Printf("⬜")
+					abort = true
+				case 0:
+					fmt.Printf("⬛")
+					abort = true
+				}
+				if abort {
+					break
+				}
+			}
+			if !abort {
+				panic("eek")
+			}
+		}
+		fmt.Printf("\n")
+	}
+}
+
+func (image SpaceImage) PrintImage2() {
+	finalImage := make([]byte, image.width*image.height)
+	for i := 0; i < image.width*image.height; i++ {
+		finalImage[i] = 2
+	}
+	for li := len(image.layers) - 1; li >= 0; li-- {
+		for i := 0; i < image.width*image.height; i++ {
+			layer := image.layers[li]
+
+			if layer[i] == 2 {
+				continue
+			}
+			finalImage[i] = layer[i]
+		}
+	}
+
+	for y := 0; y < image.height; y++ {
+		for x := 0; x < image.width; x++ {
+			i := y*image.width + x
+			switch finalImage[i] {
+			case 2:
+				panic("eek")
+			case 1:
+				fmt.Printf("⬜")
+			case 0:
+				fmt.Printf("⬛")
+			}
+		}
+		fmt.Printf("\n")
+	}
 }
--- day8_test.go
+++ day8_test.go
@@ -7,8 +7,20 @@ import (
 
 func TestDay8_1(t *testing.T) {
 	input, _ := ioutil.ReadFile("challenge8-input.txt")
-	result := Challenge8_1(input)
+	result := Challenge8_1(input, 25, 6)
 	if result != "2500" {
 		t.Errorf("Result was incorrect, got: %s, want: %s.", result, "2500")
 	}
+}
+
+func TestDay8_2(t *testing.T) {
+	input, _ := ioutil.ReadFile("challenge8-input.txt")
+	Challenge8_2(input, 25, 6)
+	t.Errorf("Doing this so I can see the output")
+}
+
+func TestDay8_2_Example1(t *testing.T) {
+	input := []byte("0222112222120000")
+	Challenge8_2(input, 2, 2)
+	t.Errorf("Doing this so I can see the output")
 }
