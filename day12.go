413d1503dd4a44c9493c8be254b3cf8aa3ae0a19
Klazen <cmurphy1337@live.com>
2019-12-15 10:37:52 -0600

    Day 12:1 Complete

--- day12.go
+++ day12.go
@@ -1,5 +1,120 @@
 package main
 
-func Challenge12_1(input string) string {
-	return ""
+import "fmt"
+
+func Challenge12_1() string {
+	io := MoonAt("io", 5, 4, 4)
+	europa := MoonAt("europa", -11, -11, -3)
+	ganymede := MoonAt("ganymede", 0, 7, 0)
+	callisto := MoonAt("callisto", -13, 2, 10)
+
+	moons := []Body{io, europa, ganymede, callisto}
+	for i := 0; i < 1000; i++ {
+		moons = Tick(moons)
+	}
+	//fmt.Printf("%+v", moons)
+	totEnergy := 0.0
+	for _, moon := range moons {
+		totEnergy += moon.TotalEnergy()
+	}
+	return fmt.Sprintf("%.4f", totEnergy)
+}
+
+func MoonAt(name string, x, y, z float64) Body {
+	return Body{name, Vector3{x, y, z}, Vector3{0, 0, 0}}
+}
+
+type Body struct {
+	name     string
+	position Vector3
+	velocity Vector3
+}
+
+type Vector3 struct {
+	x float64
+	y float64
+	z float64
+}
+
+func Tick(world []Body) []Body {
+	//Apply gravity
+	for ai, _ := range world {
+		for bi, _ := range world {
+			if ai >= bi { //skip already processed pairs & self pairing
+				continue
+			}
+			//fmt.Printf("%d,%d -> BEFORE a: %v b: %v\n", ai, bi, world[ai], world[bi])
+			world[ai], world[bi] = Attract(world[ai], world[bi])
+			//fmt.Printf("%d,%d -> AFTER  a: %v b: %v\n", ai, bi, world[ai], world[bi])
+		}
+	}
+
+	//Apply velocity
+	for ai, _ := range world {
+		world[ai] = Advance(world[ai])
+	}
+	return world
+}
+
+func (moon Body) PotentialEnergy() float64 {
+	return Absf(moon.position.x) + Absf(moon.position.y) + Absf(moon.position.z)
+}
+
+func (moon Body) KineticEnergy() float64 {
+	return Absf(moon.velocity.x) + Absf(moon.velocity.y) + Absf(moon.velocity.z)
+}
+
+func (moon Body) TotalEnergy() float64 {
+	return moon.KineticEnergy() * moon.PotentialEnergy()
+}
+
+func Absf(a float64) float64 {
+	if a < 0 {
+		return -a
+	}
+	return a
+}
+
+func Advance(moon Body) Body {
+	moon.position.x += moon.velocity.x
+	moon.position.y += moon.velocity.y
+	moon.position.z += moon.velocity.z
+	return moon
+}
+
+func Attract(moonA Body, moonB Body) (Body, Body) {
+	moonA, moonB = AttractX(moonA, moonB)
+	moonA, moonB = AttractY(moonA, moonB)
+	moonA, moonB = AttractZ(moonA, moonB)
+	return moonA, moonB
+}
+
+func AttractP(posA float64, posB float64) (float64, float64) {
+	if posA < posB {
+		return 1, -1
+	} else if posA > posB {
+		return -1, 1
+	}
+	return 0, 0
+}
+
+func AttractX(moonA Body, moonB Body) (Body, Body) {
+	adjAX, adjBX := AttractP(moonA.position.x, moonB.position.x)
+	moonA.velocity.x += adjAX
+	moonB.velocity.x += adjBX
+	return moonA, moonB
+}
+
+func AttractY(moonA Body, moonB Body) (Body, Body) {
+	adjAY, adjBY := AttractP(moonA.position.y, moonB.position.y)
+	moonA.velocity.y += adjAY
+	moonB.velocity.y += adjBY
+	return moonA, moonB
+}
+
+func AttractZ(moonA Body, moonB Body) (Body, Body) {
+	adjAZ, adjBZ := AttractP(moonA.position.z, moonB.position.z)
+	moonA.velocity.z += adjAZ
+	moonB.velocity.z += adjBZ
+	return moonA, moonB
 }
--- day12_test.go
+++ day12_test.go
@@ -3,8 +3,147 @@ package main
 import "testing"
 
 func TestDay12_1(t *testing.T) {
-	output := Challenge12_1("")
-	if output != "idk" {
-		t.Errorf("Expected idk, got %s", output)
+	output := Challenge12_1()
+	if output != "10845.0000" {
+		t.Errorf("Expected 10845.0000, got %s", output)
+	}
+}
+
+func TestAttractPPos(t *testing.T) {
+	adj1, adj2 := AttractP(3, 5)
+	if adj1 != 1 {
+		t.Errorf("Expected adj to be 1, got %f", adj1)
+	}
+	if adj2 != -1 {
+		t.Errorf("Expected adj to be -1, got %f", adj2)
+	}
+}
+
+func TestAttractPNeg(t *testing.T) {
+	adj1, adj2 := AttractP(5, 3)
+	if adj1 != -1 {
+		t.Errorf("Expected adj to be -1, got %f", adj1)
+	}
+	if adj2 != 1 {
+		t.Errorf("Expected adj to be 1, got %f", adj2)
+	}
+}
+
+func TestAttractPZero(t *testing.T) {
+	adj1, adj2 := AttractP(5, 5)
+	if adj1 != 0 {
+		t.Errorf("Expected adj to be 0, got %f", adj1)
+	}
+	if adj2 != 0 {
+		t.Errorf("Expected adj to be 0, got %f", adj2)
+	}
+}
+
+func TestAttractX(t *testing.T) {
+	moonA, moonB := AttractX(
+		Body{"", Vector3{1, 0, 0}, Vector3{0, 0, 0}},
+		Body{"", Vector3{2, 0, 0}, Vector3{0, 0, 0}},
+	)
+	if moonA.velocity.x != 1 {
+		t.Errorf("Expected moon A X velocity to be 1, got %f", moonA.velocity.x)
+	}
+	if moonB.velocity.x != -1 {
+		t.Errorf("Expected moon B X velocity to be -1, got %f", moonB.velocity.x)
+	}
+}
+
+func TestAttractY(t *testing.T) {
+	moonA, moonB := AttractY(
+		Body{"", Vector3{0, 1, 0}, Vector3{0, 0, 0}},
+		Body{"", Vector3{0, 2, 0}, Vector3{0, 0, 0}},
+	)
+	if moonA.velocity.y != 1 {
+		t.Errorf("Expected moon A Y velocity to be 1, got %f", moonA.velocity.y)
+	}
+	if moonB.velocity.y != -1 {
+		t.Errorf("Expected moon B Y velocity to be -1, got %f", moonB.velocity.y)
+	}
+}
+
+func TestAttractZ(t *testing.T) {
+	moonA, moonB := AttractZ(
+		Body{"", Vector3{0, 0, 1}, Vector3{0, 0, 0}},
+		Body{"", Vector3{0, 0, 2}, Vector3{0, 0, 0}},
+	)
+	if moonA.velocity.z != 1 {
+		t.Errorf("Expected moon A Z velocity to be 1, got %f", moonA.velocity.z)
+	}
+	if moonB.velocity.z != -1 {
+		t.Errorf("Expected moon B Z velocity to be -1, got %f", moonB.velocity.z)
+	}
+}
+
+func TestTick(t *testing.T) {
+	moons := []Body{
+		Body{"", Vector3{0, 0, 1}, Vector3{0, 0, 0}},
+		Body{"", Vector3{0, 0, 2}, Vector3{0, 0, 0}},
+	}
+	moons = Tick(moons)
+	if moons[0].velocity.z != 1 {
+		t.Errorf("Expected moon A Z velocity to be 1, got %f", moons[0].velocity.z)
+	}
+	if moons[1].velocity.z != -1 {
+		t.Errorf("Expected moon B Z velocity to be -1, got %f", moons[1].velocity.z)
+	}
+	if moons[0].position.z != 2 {
+		t.Errorf("Expected moon A Z position to be 2, got %f", moons[0].position.z)
+	}
+	if moons[1].position.z != 1 {
+		t.Errorf("Expected moon B Z position to be 1, got %f", moons[1].position.z)
+	}
+}
+
+func TestTick4(t *testing.T) {
+	moons := []Body{
+		MoonAt("0", -1, 0, 2),
+		MoonAt("1", 2, -10, -7),
+		MoonAt("2", 4, -8, 8),
+		MoonAt("3", 3, 5, -1),
+	}
+	moons = Tick(moons)
+	if moons[0].position != (Vector3{2, -1, 1}) {
+		t.Errorf("Expected moon 1 position %v, got %v", (Vector3{2, -1, 1}), moons[0].position)
+	}
+	if moons[0].velocity != (Vector3{3, -1, -1}) {
+		t.Errorf("Expected moon 1 velocity %v, got %v", (Vector3{3, -1, -1}), moons[0].velocity)
+	}
+}
+
+func TestEnergy(t *testing.T) {
+	moon := Body{"", Vector3{2, 1, 3}, Vector3{-3, 2, 1}}
+	if moon.TotalEnergy() != 36 {
+		t.Errorf("Expected moon NRG to be 36, got %f", moon.TotalEnergy())
+	}
+}
+
+func Test10(t *testing.T) {
+	io := MoonAt("io", -8, -10, 0)
+	europa := MoonAt("europa", 5, 5, 10)
+	ganymede := MoonAt("ganymede", 2, -7, 3)
+	callisto := MoonAt("callisto", 9, -8, -3)
+
+	moons := []Body{io, europa, ganymede, callisto}
+	for i := 0; i < 10; i++ {
+		moons = Tick(moons)
+	}
+
+	if moons[0].position != (Vector3{-9, -10, 1}) {
+		t.Errorf("Expected moon 1 at %v, got %v", (Vector3{-9, -10, 1}), moons[0].position)
+	}
+}
+
+func TestAttract2(t *testing.T) {
+	left := MoonAt("l", 0, 0, 0)
+	mid := MoonAt("m", 1, 0, 0)
+	right := MoonAt("r", 2, 0, 0)
+	left, mid = Attract(left, mid)
+	left, right = Attract(left, right)
+	if left.velocity.x != 2.0 {
+		t.Errorf("Expected left moon velocity %f, got %f", 2.0, left.velocity.x)
 	}
 }
